e2402181fa068d2d29c6372a30cbb1fb2a14cc32
marceloAPP <marcelo_freeday@yahoo.com.br>
2021-03-02 20:31:44 -0300

    Inicializando Projeto

---
+++ 199_Exerc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _199_Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
