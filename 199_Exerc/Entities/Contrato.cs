089d765e771a8f2362d96934f23be41913bbc66d
marceloAPP <marcelo_freeday@yahoo.com.br>
2021-03-02 21:36:30 -0300

    Implementando as entidades contrato e prestacao

---
+++ 199_Exerc/Entities/Contrato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _199_Exerc.Entities
+{
+    class Contrato
+    {
+        public int Numero { get; set; }
+        public DateTime Data { get; set; }
+        public double ValorTotal { get; set; }
+
+        public Contrato(int numero, DateTime data, double valorTotal)
+        {
+            Numero = numero;
+            Data = data;
+            ValorTotal = valorTotal;
+        }
+
+    }
+}
---
+++ 199_Exerc/Entities/Prestacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _199_Exerc.Entities
+{
+    class Prestacao
+    {
+        public DateTime DataVencimento { get; set; }
+        public double Quantidade { get; set; }
+
+        public Prestacao(DateTime dataVencimento, double quantidade)
+        {
+            DataVencimento = dataVencimento;
+            Quantidade = quantidade;
+        }
+    }
+}
